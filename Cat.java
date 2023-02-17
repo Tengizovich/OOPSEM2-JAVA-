a80e249b174a5717416666141389d3e3cb1ae553
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-02-17 22:31:27 +0300

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends PetAnimal{
+    public Cat(String nickName, int age){
+        super(nickName, age);
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("Meow-meow-meow");
+    }
+
+}
