bb6de1456fedef264f3cfa2356c5da779157f202
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-02-17 22:33:23 +0300

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends PetAnimal{
+
+    public Dog(String nickName, int age) {
+        super(nickName, age);
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("Woof-woof");
+    }
+}
